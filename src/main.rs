6a36ad65cbfb5109d168adde26c1891adef94ef5
adilrana03 <adilrana@minnotra.com>
2024-05-22 01:17:10 +0530

    variables and forloop done

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let x = 5;
+    let y: u32 = 10;
+    let z: f32 = 20.10;
+
+    println!("value of x = {},y:{} , z:{}", x,y,z);
+    println!("{}",z);
+
+
+    println!("Hello, world!, How are you");
+    let mut num = 5;
+
+    for i in 0..num {
+        num = num +1;
+        println!("{}",num);
+    }
+    print!("{}",num);
+}
